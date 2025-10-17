26d907e0ecef6c2dc30a4a9903fdfb8d47c5e119
xxecond <andrewsampadu9@gmail.com>
2025-10-17 05:55:26 +0000

    project update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+ /** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["res.cloudinary.com"], // ✅ allow Cloudinary URLs
+  },
+};
 
 export default nextConfig;
