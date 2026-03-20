cf71d2ee0c2e864a626e32009f579c8e7ff64721
xxecond <andrewsampadu9@gmail.com>
2026-03-20 10:52:27 +0000

    project update

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,26 +1,11 @@
-{/*"use client"
-
-import { BlogCard, Navbar } from "@/components"
+"use client"
 
 export default function Test (){
-    const mockdata ={
-        _id: "1",
-        title: "My first blog",
-        content:"hello kldasjflkjldfkj",
-        createdAt: new Date().toISOString(),
-    };
-
-    const handleDelete = (id) =>{
-        console.log("Delete blog wih id:", id);
-    };
 
     return(
         <>
-        <Navbar  />
-        <div className="flex flex-col justify-center items-center bg-red-600 min-h-dvh">
-            <BlogCard blog={mockdata} onDelete={handleDelete} />               
+        <div>
             <p className="">hello</p>
             </div></>
     );
 }
- */}
